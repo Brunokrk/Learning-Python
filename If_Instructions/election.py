762ab8df4abc309cc301186044ed8e671047df04
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-03-31 21:39:21 -0300

    Aprendendo Funcionamento IF

---
+++ If_Instructions/election.py
@@ -0,0 +1,10 @@
+age = 16
+if (age >= 18):
+    print("Idade mínima de voto obrigatório alcançada")
+elif (age<18 and age >=16):
+    print("Voto nao obrigatório permitido")
+else:
+    print("Ainda faltam " + str(16-age)+" anos para permissão de voto")
+
+
+
---
+++ If_Instructions/testes_condicionais.py
@@ -0,0 +1,5 @@
+# = atribuição, == comparação
+car='panamera' 
+print(car=='panamera')
+car='audi'
+print(car=='panamera')
