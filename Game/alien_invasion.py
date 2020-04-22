7b16cef70d8629732ef2a20c648460bf809b125d
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-04-22 15:40:45 -0300

    agr a nave anda

--- Game/alien_invasion.py
+++ Game/alien_invasion.py
@@ -1,21 +1,35 @@
-import sys
-
 import pygame
 
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+
 def run_game():
-    """Inicializa o jogo e cria um objeto na tela"""
-    pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    """Inicializa o pygame, as configurações e o objeto screen"""
+
+    #inicializa configurações de segundo plano necessárias ao pygame
+    #para funcionar apropriadamente
+    pygame.init()   
+
+    #Instancia de settings para acessar configurações
+    ai_settings = Settings()
+
+    #cria janela de exibição
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) #
     pygame.display.set_caption("Space Invaders")
+
+    #Cria uma espaçonave
+    ship = Ship(screen)
+
 
     #Inicializa o laço principal do jogo
     while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
-        #Observa eventos de teclado e mouse
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()  
-        #Deixa a tela mais recente visível
-        pygame.display.flip()
+        
 
 run_game()
