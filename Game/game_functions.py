350c71f20fd6330e61f4fd3cbdeeb4b08427fcb4
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-04-23 16:01:38 -0300

    Implementado os Projéteis da nave

--- Game/game_functions.py
+++ Game/game_functions.py
@@ -1,13 +1,25 @@
 import sys
 
+from bullet import Bullet
+
 import pygame
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Responde a ressionamentos de teclas"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        #Cria um novo projétil e adiciona ao grupo de projéteis
+        fire_bullets(ai_settings, screen, ship, bullets)
+
+
+def fire_bullets(ai_settings, screen, ship, bullets):
+    """Dispara um projétil se o limite ainda nao foi alcançado."""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Responde a soltura de teclas"""
@@ -15,26 +27,41 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
-def check_events(ship):
-    """Responde a eventos de pressionamento de tecls e de mouse"""
+
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Responde a eventos de pressionamento de teclas e de mouse"""
 
     for event in pygame.event.get():#Laço de eventos
         if event.type == pygame.QUIT:
              sys.exit()  
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Atualiza as imagens na tela e alterna para a nova tela"""
     #Redesenha a tela a cada passagem pelo laço
     screen.fill(ai_settings.bg_color)
+
+    #Redesenha todos os projéteis atrás da espaçonave e dos alienigenas
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     #Deixa a tela mais recente visível
     pygame.display.flip()
 
 
+def update_bullets (bullets):
+
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <=0:
+            bullets.remove(bullet)
+    
+    
--- Game/settings.py
+++ Game/settings.py
@@ -5,10 +5,16 @@ class Settings():
         """Inicializa as configurações do jogo"""
 
         #Configurações da tela
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1200 #Tamanho da tela
+        self.screen_height = 800    #Tamanho da tela
         self.bg_color = (230, 230, 230) #background color
 
         #Configurações da nave
-        self.ship_speed_factor = 1.5
-        
+        self.ship_speed_factor = 1.5 #Velocidade da nave (pixels por clique)
+
+        #Configurações dos projéteis
+        self.bullet_speed_factor = 1 #Velocidade projétil
+        self.bullet_width = 3   #Largura tiro
+        self.bullet_height = 15 #Altura tiro
+        self.bullet_color = 60, 60, 60  #Cor do tiro
+        self.bullets_allowed = 3    #Limite de tiros
