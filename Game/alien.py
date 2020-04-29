b4f506a527da1954ac3702df0682b92a63878f27
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-04-29 13:09:41 -0300

    final

--- Game/alien.py
+++ Game/alien.py
@@ -1,31 +1,40 @@
 import pygame
-
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
-    """Uma classe que representa um unico alienígena da frota"""
+    """A class to represent a single alien in the fleet."""
 
-    def __init_(self, ai_settings, screen):
-        """Inicializa o alienígena e define sua posição inicial"""
-
+    def __init__(self, ai_settings, screen):
+        """Initialize the alien, and set its starting position."""
         super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
-        #Carrega a imagem do alienígena e define seu atributo rect
-        self.image = pygame.image.load('Game/images/alien.bmp')
+        # Load the alien image, and set its rect attribute.
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        #Inicia cada novo alienígena próximo a parte superior esquerda da tela
+        # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        #Armazena a posição exata do alienígena
+        # Store the alien's exact position.
         self.x = float(self.rect.x)
+        
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+        
+    def update(self):
+        """Move the alien right or left."""
+        self.x += (self.ai_settings.alien_speed_factor *
+                        self.ai_settings.fleet_direction)
+        self.rect.x = self.x
 
-    
     def blitme(self):
-        """Desenha o alienígena em sua posição atual"""
-
+        """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
-    
--- Game/ship.py
+++ Game/ship.py
@@ -1,39 +1,45 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
+
     def __init__(self, ai_settings, screen):
-        """Inicializa a espaçonave e define sua posição inicial"""
+        """Initialize the ship, and set its starting position."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
-        #Carrega a imagem da espaçonave e obtém seu rect
-        self.image = pygame.image.load('Game/images/space_ship.bmp')
-        self.rect = self.image.get_rect() #retangulo da espaçonave
-        self.screen_rect = screen.get_rect() #retangulo da tela
+        # Load the ship image, and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
-        #Inicia cada nova espaçonave na parte inferior central da tela
-            #coordenada X da nave coincide com coordenada X da tela
-        self.rect.centerx = self.screen_rect.centerx 
-            #coordenada Y da parte inferior da nave igual ao bottom da tela
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
-        #Armazena um valor decimal para o centro da nave
+        
+        # Store a decimal value for the ship's center.
         self.center = float(self.rect.centerx)
-
-        #Flag de movimento
+        
+        # Movement flags.
         self.moving_right = False
         self.moving_left = False
-    
+        
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
+        
     def update(self):
-        """Atualiza a posição da espaçonave de acordo com a flag de movimento"""
+        """Update the ship's position, based on movement flags."""
+        # Update the ship's center value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-        
-        #Atuaiza o objeto rect de acordo com self.center
+            
+        # Update rect object from self.center.
         self.rect.centerx = self.center
-    
+
     def blitme(self):
-        """Desenha a espaçonave em sua posição especificada por self.rect"""
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
